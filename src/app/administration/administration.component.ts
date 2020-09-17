576640f4f98f92dc596ab3f8b051d7aab5695747
drakoception <franciscocomas@iespaucasesnoves.cat>
2020-09-17 16:45:08 +0200

    [Administration] Added template and showing the data of json files

--- src/app/administration/administration.component.ts
+++ src/app/administration/administration.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from '../song';
+import { Photo } from '../photos';
+import { Biography } from '../biography';
+import songsData from '../../assets/data/songs.json'
+import photosData from '../../assets/data/photos.json'
+import bioData from '../../assets/data/biography.json'
+
 
 @Component({
   selector: 'app-administration',
@@ -6,10 +13,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administration.component.css']
 })
 export class AdministrationComponent implements OnInit {
+  songs: Song [] = songsData;
+  photos: Photo[] = photosData;
+  biography: Biography = bioData;
 
-  constructor() { }
+  songsDiv:boolean=false;
+  photosDiv:boolean=false;
+  biographyDiv:boolean=false;
+  songAdd:boolean=false;
+  photoAdd:boolean=false;
 
-  ngOnInit(): void {
+    songsDivFunction(){
+        if(!this.songsDiv){
+          this.songsDiv=true;
+        }else{
+          this.songsDiv=false;
+        }
+        this.photosDiv=false;
+        this.biographyDiv=false
+    }
+
+    photosDivFunction(){
+        if(!this.photosDiv){
+          this.photosDiv=true;
+        }else{
+          this.photosDiv=false;
+        }
+        this.songsDiv=false;
+        this.biographyDiv=false
+    }
+
+    biographyDivFunction(){
+        if(!this.biographyDiv){
+          this.biographyDiv=true;
+        }else{
+          this.biographyDiv=false;
+        }
+        this.photosDiv=false;
+        this.songsDiv=false
+    }
+    showAddSong(){
+      this.songAdd=true;
+    }
+    addSongFunction(){
+      this.songAdd=false;
+    }
+    hideAddSongFunction(){
+      this.songAdd=false;
+    }
+
+    showAddPhoto(){
+      this.photoAdd=true;
+    }
+    addPhotoFunction(){
+      this.photoAdd=false;
+    }
+    hideAddPhotoFunction(){
+      this.photoAdd=false;
+    }
+
+
+  ngOnInit(): void { 
+
   }
 
 }
---
+++ src/app/biography.ts
@@ -0,0 +1,3 @@
+export interface Biography{
+    text: string;
+}
